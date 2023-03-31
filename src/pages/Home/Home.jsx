1cc99910ba8a736e87644d29b31b738eff7552af
anaschwaab <goettensana@gmail.com>
2023-03-31 12:11:39 -0300

    alteração da home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,38 @@
+import {Card, Container } from "react-bootstrap";
+
+
+
+
+// Dica: Utilize as funções filter e propriedade length para montar as visualizações.
 export function Home() {
-  return <>HOME</>;
+  return (
+    <div className="home">
+     <Container>
+     <div className="d-flex justify-content-between align-items-center">
+     <h1>Visão Geral</h1>
+     </div>
+      <hr/>
+      <div className="d-flex justify-content-evenly align-items-center">
+      <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title className="d-flex justify-content-center">Total de empréstimos</Card.Title>
+          <Card.Text style={{color: 'green', fontWeight: "bold"}}>NUMERO</Card.Text>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title className="d-flex justify-content-center">Total de livros</Card.Title>
+          <Card.Text style={{color: 'green', fontWeight: "bold"}}>NUMERO</Card.Text>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title className="d-flex justify-content-center">Pendentes/Devolvidos</Card.Title>
+          <Card.Text style={{color: 'green', fontWeight: "bold"}}>NUMERO</Card.Text>
+        </Card.Body>
+      </Card>
+     </div>
+     </Container>
+    </div>
+  );
 }
