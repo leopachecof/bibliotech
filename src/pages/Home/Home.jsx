48feabb6842d3189604e8d2be00d6b4010b22e49
anaschwaab <goettensana@gmail.com>
2023-04-03 15:02:35 -0300

    implementação da home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,38 +1,84 @@
-import {Card, Container } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Card, Container } from "react-bootstrap";
+import { getEmprestimos } from "../../firebase/emprestimos";
+import { getLivros } from "../../firebase/livros";
+import "./Home.css";
 
-
-
-
-// Dica: Utilize as funções filter e propriedade length para montar as visualizações.
 export function Home() {
+  const [emprestimos, setEmprestimos] = useState();
+  const [livros, setLivros] = useState();
+
+  useEffect(() => {
+    getEmprestimos().then((busca) => {
+      setEmprestimos(busca);
+    });
+  }, []);
+
+  useEffect(() => {
+    getLivros().then((busca) => {
+      setLivros(busca);
+    });
+  }, []);
+
+  const pendentes = emprestimos?.filter(
+    (emprestimo) => emprestimo.status === "Pendente"
+  );
+  const devolvidos = emprestimos?.filter(
+    (emprestimo) => emprestimo.status === "Entregue"
+  );
+
   return (
     <div className="home">
-     <Container>
-     <div className="d-flex justify-content-between align-items-center">
-     <h1>Visão Geral</h1>
-     </div>
-      <hr/>
-      <div className="d-flex justify-content-evenly align-items-center">
-      <Card style={{ width: '18rem' }}>
-        <Card.Body>
-          <Card.Title className="d-flex justify-content-center">Total de empréstimos</Card.Title>
-          <Card.Text style={{color: 'green', fontWeight: "bold"}}>NUMERO</Card.Text>
-        </Card.Body>
-      </Card>
-      <Card style={{ width: '18rem' }}>
-        <Card.Body>
-          <Card.Title className="d-flex justify-content-center">Total de livros</Card.Title>
-          <Card.Text style={{color: 'green', fontWeight: "bold"}}>NUMERO</Card.Text>
-        </Card.Body>
-      </Card>
-      <Card style={{ width: '18rem' }}>
-        <Card.Body>
-          <Card.Title className="d-flex justify-content-center">Pendentes/Devolvidos</Card.Title>
-          <Card.Text style={{color: 'green', fontWeight: "bold"}}>NUMERO</Card.Text>
-        </Card.Body>
-      </Card>
-     </div>
-     </Container>
+      <Container>
+        <h1>Visão Geral</h1>
+        <hr />
+        <div className="d-flex justify-content-evenly">
+          <Card className="card-home"
+          >
+            <Card.Body>
+              <Card.Title>Total de empréstimos</Card.Title>
+              <Card.Text
+                style={{ color: "green", fontWeight: "bold", fontSize: 50 }}
+                className="d-flex justify-content-center"
+              >
+                {emprestimos ? emprestimos.length : 0}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+
+          <Card className="card-home"
+          >
+            <Card.Body>
+              <Card.Title>Total de livros</Card.Title>
+              <Card.Text
+                style={{ color: "green", fontWeight: "bold", fontSize: 50 }}
+                className="d-flex justify-content-center"
+              >
+                {livros ? livros.length : 0}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+
+          <Card className="card-home"
+          >
+            <Card.Body>
+              <Card.Title>Relação de empréstimos</Card.Title>
+              <Card.Text
+                style={{ color: "green", fontWeight: "bold", fontSize: 25 }}
+                className="d-flex justify-content-center"
+              >
+                {`${devolvidos ? devolvidos.length : 0} devolvidos`}
+              </Card.Text>
+              <Card.Text
+                style={{ color: "#eead2d", fontWeight: "bold", fontSize: 25 }}
+                className="d-flex justify-content-center"
+              >
+                {`${pendentes ? pendentes.length : 0} pendentes`}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+      </Container>
     </div>
   );
 }
