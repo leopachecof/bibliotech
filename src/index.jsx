117f479ca119d62c4e1f00b1140a7e53be2e5f0e
leopachecof <l.nikiti@gmail.com>
2023-04-04 10:17:59 -0300

    Customização do Bootstrap com uso do Sass

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,9 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
-import "./index.css";
+import "./index.scss";
+
+
+
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
