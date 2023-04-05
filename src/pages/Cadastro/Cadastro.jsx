437f1867f6aa9233977dc6e38a4e254434364f6f
gabiaraga <gabiaragao.ti@gmail.com>
2023-04-05 10:45:10 -0300

    implementaçao de Footer e ocultar/mostrar senha

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -1,17 +1,28 @@
-import { Button, Container, Form, OverlayTrigger, Tooltip } from "react-bootstrap";
+import {
+  Button,
+  Container,
+  Form,
+  OverlayTrigger,
+  Tooltip,
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
 import logoIcon from "../../assets/icons/livros.png";
 import googleIcon from "../../assets/icons/google-white.svg";
 import { useForm } from "react-hook-form";
-import { cadastrarEmailSenha, loginGitHub, loginGoogle, loginfacebook } from "../../firebase/auth";
+import {
+  cadastrarEmailSenha,
+  loginGitHub,
+  loginGoogle,
+  loginfacebook,
+} from "../../firebase/auth";
 import { toast } from "react-hot-toast";
 import { useNavigate, Navigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
 import FacebookIcon from "../../assets/icons/facebookIcon.png";
 import GitHubIcon from "../../assets/icons/githubicon.png";
-
-
+import { Footer } from "../../components/Footer/Footer";
+import { useState } from "react";
 
 export function Cadastro() {
   const {
@@ -22,6 +33,9 @@ export function Cadastro() {
 
   const navigate = useNavigate();
   const usuarioLogado = useContext(AuthContext);
+
+  const [mostrarSenha, setMostrarSenha] = useState(false);
+  const toggleMostrarSenha = () => setMostrarSenha(!mostrarSenha);
 
   // condicional para redirecionar o usuário para home caso esteja logado
   if (usuarioLogado !== null) {
@@ -47,7 +61,7 @@ export function Cadastro() {
   }
   function onLoginGitHub() {
     loginGitHub()
-    .then((user) => {
+      .then((user) => {
         toast.success(`Bem-vindo(a) ${user.displayName}`, {
           position: "bottom-right",
           duration: 2500,
@@ -94,69 +108,83 @@ export function Cadastro() {
           duration: 2500,
         });
       });
-
-
   }
 
-
-
   return (
-    <Container fluid className="my-5">
-      <p className="text-center">
-        <img src={logoIcon} width="256" alt="Logo do app" />
-      </p>
-      <h4>Faça parte da nossa plataforma</h4>
-      <p className="text-muted">
-        Já tem conta? <Link to="/login">Entre</Link>
-      </p>
-      <hr />
-      <Button className="mb-3" variant="danger" onClick={onLoginGoogle}>
-        <img src={googleIcon} width="32" alt="Logo do google" />
-        Entrar com o Google
-      </Button>
-      <Button className="mb-3 ms-3" variant="primary" onClick={onLoginFacebook}>
-        <img src={FacebookIcon} width="32" alt="Facebook icon" /> Entrar com o
-        Facebook
-      </Button>
-      <Button className="mb-3 ms-3" variant="secondary" onClick={onLoginGitHub}>
-        <img src={GitHubIcon} width="32" alt="Github icon" /> Entrar com o
-        GitHub
-      </Button>
+    <>
+      <Container fluid className="my-5">
+        <p className="text-center">
+          <img src={logoIcon} width="256" alt="Logo do app" />
+        </p>
+        <h4>Faça parte da nossa plataforma</h4>
+        <p className="text-muted">
+          Já tem conta? <Link to="/login">Entre</Link>
+        </p>
+        <hr />
+        <Button className="mb-3" variant="danger" onClick={onLoginGoogle}>
+          <img src={googleIcon} width="32" alt="Logo do google" />
+          Entrar com o Google
+        </Button>
+        <Button
+          className="mb-3 ms-3"
+          variant="primary"
+          onClick={onLoginFacebook}
+        >
+          <img src={FacebookIcon} width="32" alt="Facebook icon" /> Entrar com o
+          Facebook
+        </Button>
+        <Button
+          className="mb-3 ms-3"
+          variant="secondary"
+          onClick={onLoginGitHub}
+        >
+          <img src={GitHubIcon} width="32" alt="Github icon" /> Entrar com o
+          GitHub
+        </Button>
 
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <Form.Group className="mb-3" controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type="email"
-            className={errors.email && "is-invalid"}
-            placeholder="Seu email"
-            {...register("email", { required: "O email é obrigatório" })}
-          />
-          <Form.Text className="invalid-feedback">
-            {errors.email?.message}
-          </Form.Text>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="password">
-          <Form.Label>Senha</Form.Label>
-          <Form.Control
-            type="password"
-            className={errors.senha && "is-invalid"}
-            placeholder="Sua senha"
-            {...register("senha", { required: "A senha é obrigatória" })}
-          />
-          <Form.Text className="invalid-feedback">
-            {errors.senha?.message}
-          </Form.Text>
-        </Form.Group>
-        <OverlayTrigger
-          placement="right"
-          overlay={
-            <Tooltip>Clique aqui para cadastrar</Tooltip>}>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Form.Group className="mb-3" controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              className={errors.email && "is-invalid"}
+              placeholder="Seu email"
+              {...register("email", { required: "O email é obrigatório" })}
+            />
+            <Form.Text className="invalid-feedback">
+              {errors.email?.message}
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="password">
+            <Form.Label>Senha</Form.Label>
+            <Form.Control
+              type={mostrarSenha ? "text" : "password"}
+              placeholder="Sua senha"
+              className={errors.senha ? "is-invalid" : ""}
+              {...register("senha", { required: "Senha é obrigatória" })}
+            />
+            <Form.Text className="invalid-feedback">
+              {errors.senha?.message}
+            </Form.Text>
+            <button
+              type="button"
+              class="btn btn-secondary btn-sm"
+              onClick={toggleMostrarSenha}
+            >
+              {mostrarSenha ? "Ocultar Senha" : "Mostrar Senha"}
+            </button>
+          </Form.Group>
+          <OverlayTrigger
+            placement="right"
+            overlay={<Tooltip>Clique aqui para cadastrar</Tooltip>}
+          >
             <Button type="submit" variant="success">
               Cadastrar
             </Button>
-        </OverlayTrigger>
-      </Form>
-    </Container>
+          </OverlayTrigger>
+        </Form>
+      </Container>
+      <Footer />
+    </>
   );
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,11 @@
 import { useContext } from "react";
-import { Button, Container, Form, OverlayTrigger, Tooltip } from "react-bootstrap";
+import {
+  Button,
+  Container,
+  Form,
+  OverlayTrigger,
+  Tooltip,
+} from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
 import { Link, Navigate, useNavigate } from "react-router-dom";
@@ -8,9 +14,14 @@ import FacebookIcon from "../../assets/icons/facebookIcon.png";
 import GitHubIcon from "../../assets/icons/githubicon.png";
 import loginImg from "../../assets/images/login.png";
 import { AuthContext } from "../../contexts/AuthContext";
-import { loginGoogle, loginEmailSenha, loginfacebook, loginGitHub } from "../../firebase/auth";
-
-
+import {
+  loginGoogle,
+  loginEmailSenha,
+  loginfacebook,
+  loginGitHub,
+} from "../../firebase/auth";
+import { Footer } from "../../components/Footer/Footer";
+import { useState } from "react";
 
 export function Login() {
   const {
@@ -21,9 +32,11 @@ export function Login() {
 
   const navigate = useNavigate();
 
+  const [mostrarSenha, setMostrarSenha] = useState(false);
+  const toggleMostrarSenha = () => setMostrarSenha(!mostrarSenha);
 
   function onSubmit(data) {
-    const {email, senha } = data;
+    const { email, senha } = data;
     loginEmailSenha(email, senha)
       .then((user) => {
         toast.success(`Entrando como ${user.email}`, {
@@ -55,8 +68,6 @@ export function Login() {
           duration: 2500,
         });
       });
-
-
   }
 
   function onLoginFacebook() {
@@ -77,7 +88,7 @@ export function Login() {
   }
   function onLoginGitHub() {
     loginGitHub()
-    .then((user) => {
+      .then((user) => {
         toast.success(`Bem-vindo(a) ${user.displayName}`, {
           position: "bottom-right",
           duration: 2500,
@@ -100,64 +111,82 @@ export function Login() {
   }
 
   return (
-    <Container fluid className="my-5">
-      <p className="text-center">
-        <img src={loginImg} width="256" alt="Logo" />
-      </p>
-      <h4>Bem-vindo(a) de volta!</h4>
-      <p className="text-muted">
-        Não tem conta? <Link to="/cadastro">Cadastre-se</Link>
-      </p>
-      <hr />
+    <>
+      <Container fluid className="my-5">
+        <p className="text-center">
+          <img src={loginImg} width="256" alt="Logo" />
+        </p>
+        <h4>Bem-vindo(a) de volta!</h4>
+        <p className="text-muted">
+          Não tem conta? <Link to="/cadastro">Cadastre-se</Link>
+        </p>
+        <hr />
 
-      <Button className="mb-3" variant="danger" onClick={onLoginGoogle}>
-        <img src={googleIcon} width="32" alt="Google icon" /> Entrar com o
-        Google
-      </Button>
+        <Button className="mb-3" variant="danger" onClick={onLoginGoogle}>
+          <img src={googleIcon} width="32" alt="Google icon" /> Entrar com o
+          Google
+        </Button>
 
-      <Button className="mb-3 ms-3" variant="primary" onClick={onLoginFacebook}>
-        <img src={FacebookIcon} width="32" alt="Facebook icon" /> Entrar com o
-        Facebook
-      </Button>
-      <Button className="mb-3 ms-3" variant="secondary" onClick={onLoginGitHub}>
-        <img src={GitHubIcon} width="32" alt="Github icon" /> Entrar com o
-        GitHub
-      </Button>
+        <Button
+          className="mb-3 ms-3"
+          variant="primary"
+          onClick={onLoginFacebook}
+        >
+          <img src={FacebookIcon} width="32" alt="Facebook icon" /> Entrar com o
+          Facebook
+        </Button>
+        <Button
+          className="mb-3 ms-3"
+          variant="secondary"
+          onClick={onLoginGitHub}
+        >
+          <img src={GitHubIcon} width="32" alt="Github icon" /> Entrar com o
+          GitHub
+        </Button>
 
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <Form.Group className="mb-3" controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Seu email"
-            className={errors.email ? "is-invalid" : ""}
-            {...register("email", { required: "Email é obrigatório" })}
-          />
-          <Form.Text className="invalid-feedback">
-            {errors.email?.message}
-          </Form.Text>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="senha">
-          <Form.Label>Senha</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Sua senha"
-            className={errors.senha ? "is-invalid" : ""}
-            {...register("senha", { required: "Senha é obrigatória" })}
-          />
-          <Form.Text className="invalid-feedback">
-            {errors.senha?.message}
-          </Form.Text>
-        </Form.Group>
-        <OverlayTrigger
-          placement="right"
-          overlay={
-            <Tooltip>Clique aqui para entrar</Tooltip>}>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Form.Group className="mb-3" controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Seu email"
+              className={errors.email ? "is-invalid" : ""}
+              {...register("email", { required: "Email é obrigatório" })}
+            />
+            <Form.Text className="invalid-feedback">
+              {errors.email?.message}
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="senha">
+            <Form.Label>Senha</Form.Label>
+            <Form.Control
+              type={mostrarSenha ? "text" : "password"}
+              placeholder="Sua senha"
+              className={errors.senha ? "is-invalid" : ""}
+              {...register("senha", { required: "Senha é obrigatória" })}
+            />
+            <button
+              type="button"
+              class="btn btn-secondary btn-sm"
+              onClick={toggleMostrarSenha}
+            >
+              {mostrarSenha ? "Ocultar Senha" : "Mostrar Senha"}
+            </button>
+            <Form.Text className="invalid-feedback">
+              {errors.senha?.message}
+            </Form.Text>
+          </Form.Group>
+          <OverlayTrigger
+            placement="right"
+            overlay={<Tooltip>Clique aqui para entrar</Tooltip>}
+          >
             <Button type="submit" variant="success">
               Entrar
             </Button>
-        </OverlayTrigger>
-      </Form>
-    </Container>
+          </OverlayTrigger>
+        </Form>
+      </Container>
+      <Footer />
+    </>
   );
 }
