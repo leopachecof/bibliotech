a3a721ec6631d4491d0bd812951cca7b19e6a850
anaschwaab <goettensana@gmail.com>
2023-04-05 15:48:17 -0300

    task20

--- src/pages/EditarLivro/EditarLivro.jsx
+++ src/pages/EditarLivro/EditarLivro.jsx
@@ -1,17 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
 import { useNavigate, useParams } from "react-router-dom";
 import { getLivro, updateLivro, uploadCapaLivro } from "../../firebase/livros";
-
-
+import { getAutores, getAutor } from "../../firebase/autores";
 
 
 export function EditarLivro() {
 
     const {id} = useParams();
-
+    const [autores, setAutores] = useState([]);
     const {register, handleSubmit, formState: {errors}, reset} = useForm();
     const navigate = useNavigate();
 
@@ -23,20 +22,27 @@ export function EditarLivro() {
                 toast.dismiss(toastId);
                 data.urlCapa = url;
                 delete data.imagem;
+                getAutor(data.idAutor).then(autor => {
+                    delete data.idAutor;
+                    let novoLivro = {...data, autor}
                 updateLivro(id, data).then(() => {
                     toast.success("Livro editado com sucesso!", {duration: 2000, position: "bottom-right"})
                     navigate("/livros");
                 })
             })
-        }
+        })
+    }
         else {
             delete data.imagem;
+            getAutor(data.idAutor).then(autor => {
+                delete data.idAutor;
+                let novoLivro = {...data, autor}
             updateLivro(id, data).then(() => {
                 toast.success("Livro editado com sucesso!", {duration: 2000, position: "bottom-right"})
                 navigate("/livros");
             })
-        }
-        
+        })
+    }  
     }
 
     useEffect(() => {
@@ -44,6 +50,13 @@ export function EditarLivro() {
             reset(livro);
         })
     }, [id, reset]);
+
+    useEffect(() => {
+        getAutores().then(resultado => {
+            setAutores(resultado);
+            console.log(resultado)
+        });
+    }, [])
 
     return (
         <div className="editar-livro">
@@ -60,9 +73,11 @@ export function EditarLivro() {
                     </Form.Group>
                     <Form.Group className="mb-3">
                         <Form.Label>Autor</Form.Label>
-                        <Form.Control type="text" className={errors.autor && "is-invalid"} {...register("autor", {required: "Autor é obrigatório!", maxLength: {value: 255, message: "Limite de 255 caracteres!"}})} />
-                        <Form.Text className="text-danger">
-                            {errors.autor?.message}
+                        <Form.Select  className={errors.idAutor && "is-invalid"} {...register("idAutor", {required: "Autor é obrigatório!"})}>
+                            {autores.map(autor => <option key={autor.id} value={autor.id}>{autor.nome}</option>)}
+                        </Form.Select>
+                        <Form.Text className="invalid-feedback">
+                            {errors.idAutor?.message}
                         </Form.Text>
                     </Form.Group>
                     <Form.Group className="mb-3">
