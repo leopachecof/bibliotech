42310cdd9486f48867baf775036430c22ea90440
gabiaraga <gabiaragao.ti@gmail.com>
2023-04-06 09:39:35 -0300

    Realização da task multiplas categorias de livros

--- src/pages/EditarLivro/EditarLivro.jsx
+++ src/pages/EditarLivro/EditarLivro.jsx
@@ -6,101 +6,173 @@ import { useNavigate, useParams } from "react-router-dom";
 import { getLivro, updateLivro, uploadCapaLivro } from "../../firebase/livros";
 import { getAutores, getAutor } from "../../firebase/autores";
 
-
 export function EditarLivro() {
+  const { id } = useParams();
+  const [autores, setAutores] = useState([]);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+  const navigate = useNavigate();
 
-    const {id} = useParams();
-    const [autores, setAutores] = useState([]);
-    const {register, handleSubmit, formState: {errors}, reset} = useForm();
-    const navigate = useNavigate();
+  const categorias = [
+    "Romance",
+    "Ficção Científica",
+    "Fantasia",
+    "História",
+    "Biografia",
+    "Autoajuda",
+  ];
 
-    function onSubmit(data) {
-        const imagem = data.imagem[0];
-        if(imagem) {
-            const toastId = toast.loading("Upload da imagem...", {position: "top-right"});
-            uploadCapaLivro(imagem).then(url => {
-                toast.dismiss(toastId);
-                data.urlCapa = url;
-                delete data.imagem;
-                getAutor(data.idAutor).then(autor => {
-                    delete data.idAutor;
-                    let novoLivro = {...data, autor}
-                updateLivro(id, data).then(() => {
-                    toast.success("Livro editado com sucesso!", {duration: 2000, position: "bottom-right"})
-                    navigate("/livros");
-                })
-            })
-        })
+  const [categoriaSelecionada, setCategoriaSelecionada] = useState("");
+  const [categoriasLivro, setCategoriasLivro] = useState([]);
+
+  const adicionarCategoria = () => {
+    if (
+      categoriaSelecionada &&
+      !categoriasLivro.includes(categoriaSelecionada)
+    ) {
+      setCategoriasLivro([...categoriasLivro, categoriaSelecionada]);
+      setCategoriaSelecionada("");
     }
-        else {
-            delete data.imagem;
-            getAutor(data.idAutor).then(autor => {
-                delete data.idAutor;
-                let novoLivro = {...data, autor}
-            updateLivro(id, data).then(() => {
-                toast.success("Livro editado com sucesso!", {duration: 2000, position: "bottom-right"})
-                navigate("/livros");
-            })
-        })
-    }  
-    }
+  };
 
-    useEffect(() => {
-        getLivro(id).then(livro => {
-            reset(livro);
-        })
-    }, [id, reset]);
-
-    useEffect(() => {
-        getAutores().then(resultado => {
-            setAutores(resultado);
-            console.log(resultado)
+  function onSubmit(data) {
+    const imagem = data.imagem[0];
+    if (imagem) {
+      const toastId = toast.loading("Upload da imagem...", {
+        position: "top-right",
+      });
+      uploadCapaLivro(imagem).then((url) => {
+        toast.dismiss(toastId);
+        data.urlCapa = url;
+        delete data.imagem;
+        getAutor(data.idAutor).then((autor) => {
+          delete data.idAutor;
+          let novoLivro = { ...data, autor };
+          updateLivro(id, data).then(() => {
+            toast.success("Livro editado com sucesso!", {
+              duration: 2000,
+              position: "bottom-right",
+            });
+            navigate("/livros");
+          });
         });
-    }, [])
+      });
+    } else {
+      delete data.imagem;
+      getAutor(data.idAutor).then((autor) => {
+        delete data.idAutor;
+        let novoLivro = { ...data, autor };
+        updateLivro(id, data).then(() => {
+          toast.success("Livro editado com sucesso!", {
+            duration: 2000,
+            position: "bottom-right",
+          });
+          navigate("/livros");
+        });
+      });
+    }
+  }
 
-    return (
-        <div className="editar-livro">
-            <Container>
-                <h1>Editar livro</h1>
-                <hr />
-                <Form onSubmit={handleSubmit(onSubmit)}>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Título</Form.Label>
-                        <Form.Control type="text" className={errors.titulo && "is-invalid"} {...register("titulo", {required: "Título é obrigatório!", maxLength: {value: 255, message: "Limite de 255 caracteres!"}})} />
-                        <Form.Text className="text-danger">
-                            {errors.titulo?.message}
-                        </Form.Text>
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Autor</Form.Label>
-                        <Form.Select  className={errors.idAutor && "is-invalid"} {...register("idAutor", {required: "Autor é obrigatório!"})}>
-                            {autores.map(autor => <option key={autor.id} value={autor.id}>{autor.nome}</option>)}
-                        </Form.Select>
-                        <Form.Text className="invalid-feedback">
-                            {errors.idAutor?.message}
-                        </Form.Text>
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Categoria</Form.Label>
-                        <Form.Control type="text" className={errors.categoria && "is-invalid"} {...register("categoria", {required: "Categoria é obrigatória!", maxLength: {value: 255, message: "Limite de 255 caracteres!"}})} />
-                        <Form.Text className="text-danger">
-                            {errors.categoria?.message}
-                        </Form.Text>
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>ISBN</Form.Label>
-                        <Form.Control type="text" className={errors.isbn && "is-invalid"} {...register("isbn", {required: "ISBN é obrigatório!", maxLength: {value: 255, message: "Limite de 255 caracteres!"}})} />
-                        <Form.Text className="text-danger">
-                            {errors.isbn?.message}
-                        </Form.Text>
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Imagem da capa</Form.Label>
-                        <Form.Control type="file" {...register("imagem")} />
-                    </Form.Group>
-                    <Button type="submit" variant="success">Editar</Button>
-                </Form>
-            </Container>
-        </div>
-    )
+  useEffect(() => {
+    getLivro(id).then((livro) => {
+      reset(livro);
+    });
+  }, [id, reset]);
+
+  useEffect(() => {
+    getAutores().then((resultado) => {
+      setAutores(resultado);
+      console.log(resultado);
+    });
+  }, []);
+
+  return (
+    <div className="editar-livro">
+      <Container>
+        <h1>Editar livro</h1>
+        <hr />
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Form.Group className="mb-3">
+            <Form.Label>Título</Form.Label>
+            <Form.Control
+              type="text"
+              className={errors.titulo && "is-invalid"}
+              {...register("titulo", {
+                required: "Título é obrigatório!",
+                maxLength: { value: 255, message: "Limite de 255 caracteres!" },
+              })}
+            />
+            <Form.Text className="text-danger">
+              {errors.titulo?.message}
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Autor</Form.Label>
+            <Form.Select
+              className={errors.idAutor && "is-invalid"}
+              {...register("idAutor", { required: "Autor é obrigatório!" })}
+            >
+              {autores.map((autor) => (
+                <option key={autor.id} value={autor.id}>
+                  {autor.nome}
+                </option>
+              ))}
+            </Form.Select>
+            <Form.Text className="invalid-feedback">
+              {errors.idAutor?.message}
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Categoria</Form.Label>
+
+            <Form.Select
+              value={categoriaSelecionada}
+              onChange={(e) => setCategoriaSelecionada(e.target.value)}
+            >
+              <option value="">Selecione uma categoria</option>
+              {categorias.map((categoria) => (
+                <option key={categoria} value={categoria}>
+                  {categoria}
+                </option>
+              ))}
+            </Form.Select>
+          </Form.Group>
+
+          <Button
+            type="button"
+            className="mb-3"
+            variant="success"
+            onClick={adicionarCategoria}
+          >
+            Adicionar categoria
+          </Button>
+          <Form.Group className="mb-3">
+            <Form.Label>ISBN</Form.Label>
+            <Form.Control
+              type="text"
+              className={errors.isbn && "is-invalid"}
+              {...register("isbn", {
+                required: "ISBN é obrigatório!",
+                maxLength: { value: 255, message: "Limite de 255 caracteres!" },
+              })}
+            />
+            <Form.Text className="text-danger">
+              {errors.isbn?.message}
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Imagem da capa</Form.Label>
+            <Form.Control type="file" {...register("imagem")} />
+          </Form.Group>
+          <Button type="submit" variant="success">
+            Editar
+          </Button>
+        </Form>
+      </Container>
+    </div>
+  );
 }
